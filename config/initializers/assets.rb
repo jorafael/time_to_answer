b2dad7f772b247cf00a13d4f8eafe25f49ae24dc
jorafael <jorafael@gmail.com>
2020-01-01 20:12:27 -0300

    Portando o tema AdminLTE3 para o User

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -11,4 +11,13 @@ Rails.application.config.assets.paths << Rails.root.join('node_modules')
 # Precompile additional assets.
 # application.js, application.css, and all non-JS/CSS in the app/assets
 # folder are already added.
-Rails.application.config.assets.precompile += %w( admins_backoffice.js admins_backoffice.css )
+Rails.application.config.assets.precompile += %w( 
+  admins_backoffice.js admins_backoffice.css 
+  users_backoffice.js users_backoffice.css 
+  )
+
+# lib/assests
+Rails.application.config.assets.precompile += %w( 
+  user.png 
+  siteLogo.png
+)
